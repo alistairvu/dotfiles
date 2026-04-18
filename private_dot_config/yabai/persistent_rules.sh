75894b3fd674f968569ad7dab8a6c614e0380b27
alistair <alistairvu@gmail.com>
2026-04-18 20:26:08 +1000

    add rules

--- private_dot_config/yabai/persistent_rules.sh
+++ private_dot_config/yabai/persistent_rules.sh
@@ -7,3 +7,8 @@ yabai -m rule --add app="Antinote" title="Updating Antinote" manage=off
 yabai -m rule --add app="Antinote" title="Export Markdown Note" manage=off
 yabai -m rule --add app="Antinote" title="Export" manage=off
 yabai -m rule --add app="Install Plane" title="Install Plane" manage=off
+yabai -m rule --add app="Gemini" title="Gemini sign in" manage=off
+yabai -m rule --add app="Gemini" title="Gemini onboarding" manage=off
+yabai -m rule --add app="Discord" title="Discord Updater" manage=off
+yabai -m rule --add app="Karabiner-Updater" title="Software Update" manage=off
+yabai -m rule --add app="Raycast" title="Settings" manage=off
