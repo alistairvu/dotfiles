c954b38a12fae4d5bbb060d719fdd76b735df872
alistair <alistairvu@gmail.com>
2026-04-26 11:40:22 +1000

    Update .config/mise/config.toml Update yabai_persistent_rules.sh

--- yabai_persistent_rules.sh
+++ yabai_persistent_rules.sh
@@ -38,3 +38,4 @@ yabai -m rule --add app="Tero Subtitler" title="Save file" manage=off
 yabai -m rule --add app="Tero Subtitler" title="Save subtitles" manage=off
 yabai -m rule --add app="VMware Fusion" title="Fedora 64-bit Arm" manage=off
 yabai -m rule --add app="Installer" title="Install DaVinci Resolve" manage=off
+yabai -m rule --add app="Activity Monitor" title="CPU Usage" manage=off
