03eb17004add997ca615ed2e7632fd431326b187
alistair <alistairvu@gmail.com>
2026-04-27 19:51:52 +1000

    Update .config/fish/functions/edit-packages.fish

--- private_dot_config/fish/functions/edit-packages.fish
+++ private_dot_config/fish/functions/edit-packages.fish
@@ -1,4 +1,12 @@
 function edit-packages
-    set PKG_FILE "$(chezmoi source-path)/.chezmoidata/packages.yaml"
-    vim $PKG_FILE
+  set -l os (uname)
+  if test "$os" = Darwin
+    set PKG_FILE "$(chezmoi source-path)/.chezmoidata/darwin_packages.yaml"
+  else if test "$os" = Linux and test -f /etc/arch-release
+    set PKG_FILE "$(chezmoi source-path)/.chezmoidata/arch_packages.yaml"
+  else
+    echo "Unsupported OS: $os"
+    return
+  end
+  vim $PKG_FILE
 end
